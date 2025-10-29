2840bf9c9b50df94b38a4dc88875d8dff8b2f7c3
farmwithfriends <royally.handmade@gmail.com>
2025-10-29 15:18:46 -0700

    update api URL

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -18,7 +18,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://mtg-store-api.onrender.com',
         changeOrigin: true
       }
     }
